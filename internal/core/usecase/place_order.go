e0f2372e1e1dce1ca612fe2781701127f7f2cd0d
Salo Lobato <salo.lobato@gmail.com>
2026-03-18 12:51:11 -0300

    feat: add PlaceOrder usecase

---
+++ internal/core/usecase/place_order.go
@@ -0,0 +1,46 @@
+package usecase
+
+import (
+	"errors"
+
+	"github.com/salobato/ordermanager/internal/core/entity"
+	"github.com/salobato/ordermanager/internal/core/repository"
+)
+
+type PlaceOrderInput struct {
+	CustomerID string
+	Total      float64
+}
+
+func (i PlaceOrderInput) Validate() error {
+	if i.CustomerID == "" {
+		return errors.New("O ID do cliente não pode ser vazio")
+	}
+	if i.Total <= 0 {
+		return errors.New("O total deve ser maior que zero")
+	}
+	return nil
+}
+
+func PlaceOrder(r repository.OrderRepository, input PlaceOrderInput) (*entity.Order, error) {
+	if err := input.Validate(); err != nil {
+		return nil, err
+	}
+
+	seq, err := r.NextSequence()
+	if err != nil {
+		return nil, err
+	}
+
+	entry, err := entity.NewOrder(input.CustomerID, input.Total, seq)
+	if err != nil {
+		return nil, err
+	}
+
+	order, err := r.Save(entry)
+	if err != nil {
+		return nil, err
+	}
+
+	return order, nil
+}
